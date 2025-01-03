570bd3ff2508b0d81321421875dc741134d0a64d
cgohil8 <chetan.gohil@psych.ox.ac.uk>
2025-01-03 11:00:38 +1100

    Added funcoin.__version__.

--- funcoin/__init__.py
+++ funcoin/__init__.py
@@ -4,4 +4,14 @@ normcap
 An python package for doing Functiontal Connectivity Integrative Normative Modelling.
 """
 
+from pkg_resources import DistributionNotFound, get_distribution
+
 __author__ = 'Janus R. L. Kobbersmed'
+
+# Setup the version
+try:
+    __version__ = get_distribution("funcoin").version
+except DistributionNotFound:
+    __version__ = "unknown"
+finally:
+    del get_distribution, DistributionNotFound
